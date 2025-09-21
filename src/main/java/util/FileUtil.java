76cc3fb220c9f2b83fed6104eb1e86ef6f71671d
Scaler <2623846745@qq.com>
2025-09-21 20:31:27 +0800

    feat:添加文件读取和输出的功能

---
+++ src/main/java/util/FileUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileUtil {
+
+    public static String readFile(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath))).trim();
+    }
+
+    public static void writeFile(String outputFilePath, double similarity) throws IOException {
+        String result = String.format("%.2f", similarity);
+        Files.write(Paths.get(outputFilePath), result.getBytes());
+    }
+}
